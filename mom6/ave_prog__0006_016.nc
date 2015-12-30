52992096cef672e6ff51b2107c7bfbdb91458cb1
Christopher Bull <z3457920@student.unsw.edu.au>
2015-12-30 16:35:07 +1100

    files from mom6

---
+++ mom6/ave_prog__0006_016.nc
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:63430c63c7b2961632ea6013a2fd218cfb7cf7505cc7d09d90b4f2b4a139454f
+size 37044591
---
+++ mom6/squashalice.sh
@@ -0,0 +1,8 @@
+
+set -x
+module load nco
+ncks -v PV /srv/ccrc/data01/shared/CBull/ave_prog__0006_016.nc /srv/ccrc/data01/shared/temp/ave_prog__0006_016.nc
+ncks -v PV /srv/ccrc/data01/shared/CBull/ave_prog__0007_016.nc /srv/ccrc/data01/shared/temp/ave_prog__0007_016.nc
+ncks -v PV /srv/ccrc/data01/shared/CBull/ave_prog__0008_016.nc /srv/ccrc/data01/shared/temp/ave_prog__0008_016.nc
+ncks -v PV /srv/ccrc/data01/shared/CBull/ave_prog__0009_016.nc /srv/ccrc/data01/shared/temp/ave_prog__0009_016.nc
+ncks -v PV /srv/ccrc/data01/shared/CBull/ave_prog__0010_016.nc /srv/ccrc/data01/shared/temp/ave_prog__0010_016.nc
